6017bff3dc4a16246b66cc8d2348c4dcf8d1a34f
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-04-27 23:33:16 +0300

    feat: (SignIn and Signup): create these components

---
+++ src/pages/auth/login/SignIn.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign Int</h1>
+      {/* Sign In form goes here */}
+    </div>
+  );
+} 
+export default SignIn;
---
+++ src/pages/auth/signup/Signup.jsx
@@ -0,0 +1,10 @@
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* Sign Up form goes here */}
+    </div>
+  );
+}
+
+export default SignUp;
