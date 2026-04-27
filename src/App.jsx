af7abcbc59e7e8b3158b5e9e058f21ccf89fa355
Zinedis <nantenainabakari@gmail.com>
2026-04-28 00:46:46 +0300

    feat(layouts, components, pages) : add Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NavigationLayout from "./layouts/NavigationLayout";
+import Home from "./pages/Home";
 
-function App() {
+export default function App() {
   return (
-     <div></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<NavigationLayout />}>
+          <Route path="home" element={<Home />} />
+          {/* ... autres routes */}
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
