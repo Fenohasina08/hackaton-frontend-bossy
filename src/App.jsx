38bfe86aafb72025d9d4758138b9a3c6dd0ce6d4
Zinedis <nantenainabakari@gmail.com>
2026-04-28 02:03:41 +0300

    feat(app) : range route

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavigationLayout from "./layouts/NavigationLayout";
 import Home from "./pages/Home";
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import SignIn from "./pages/auth/login/Signin";
 import SignUp from "./pages/auth/signup/Signup";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/" element={<NavigationLayout />}>
-          <Route path="home" element={<Home />} />
-          {/* ... autres routes */}
-        </Route>
-      </Routes>
-    </Router>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        {/* Pages d'Authentification (Hors du Layout) */}
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Navigate to="/" />} />
+
+        <Route path="/" element={<NavigationLayout />}>
+          {/* Redirection de la racine vers /home */}
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="home" element={<Home />} />
+          
+
+        </Route>
+
+        {/* Redirection pour les routes inconnues vers home */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
     </BrowserRouter>
   );
