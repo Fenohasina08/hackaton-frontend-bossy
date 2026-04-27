e00eca910554c334ff1335b4d7c99724160aaf65
Zinedis <nantenainabakari@gmail.com>
2026-04-27 22:55:15 +0300

    feat(src): add structur for each directory

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function Home() {
+ return (
+   <div className="min-h-screen bg-slate-950 flex items-center justify-center px-4 relative overflow-hidden text-black">
+     <p>Home</p>
+   </div>
+ );
+}
+
