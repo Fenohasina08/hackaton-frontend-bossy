8397705297bd402872423cba73cc945af4fcbc59
Miriasme <johndoe@example.com>
2026-04-28 03:36:14 +0300

    update: add layout to the universities page, modify in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import NavigationLayout from "./layouts/NavigationLayout";
-import Home from "./pages/Home";
-import Signin from "./pages/auth/login/Signin";
-import Signup from "./pages/auth/signup/Signup";
+
+import SignIn from "./pages/auth/login/Signin";
+import SignUp from "./pages/auth/signup/Signup";
 import UniversitiesPage from "./pages/UniversitiesPage";
-import Setting from "./components/Setting";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Layout principal */}
-        <Route path="/" element={<NavigationLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-
-        {/* Pages publiques */}
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/" />} />
         <Route path="/universities" element={<UniversitiesPage />} />
-        <Route path="/settings" element={<Setting />} />
-        {/* fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-
       </Routes>
     </BrowserRouter>
   );
